d80a55d08723f8259bcdd23ade12bd6f909758ec
msagatov98 <48084503+msagatov98@users.noreply.github.com>
2021-02-04 23:47:00 +0600

    waesdfgh

--- app/src/main/java/com/qazstudy/model/Message.kt
+++ app/src/main/java/com/qazstudy/model/Message.kt
@@ -4,7 +4,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.qazstudy.ui.activity.LoginActivity.Companion.mUser
 import java.util.*
 
-
 private val AUTH = FirebaseAuth.getInstance()
 
 data class Message(val userID: User = mUser, val message: String = "", val time: Long = Calendar.getInstance().timeInMillis) {
